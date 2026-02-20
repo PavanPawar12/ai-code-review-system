0f834244b9536011fd1936e50ab78310542853c3
PavanPawar12 <pavanpawar9923100@gmail.com>
2026-02-21 00:02:24 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,47 +1,4 @@
-// require('dotenv').config();
-// const app = require('./src/app')
 
-// // console.log(process.env.GOOGLE_API_KEY);
-
-// console.log("-------------------------------------->>>>>>>",process.env.GOOGLE_GEMINI_KEY)
-
-// app.listen(3000, () =>{
-//     console.log(`Server is runnning on http://localhost:3000 `)
-// })
-
-
-// import express from "express";
-// import OpenAI from "openai";
-// import dotenv from "dotenv";
- 
-// dotenv.config();
-// console.log("-------------------------->>>>>",process.env.OPENAI_API_KEY)
-
-// const app = express();
-// app.use(express.json());
-
-// const client = new OpenAI({
-//   apiKey: process.env.OPENAI_API_KEY,
-// });
-
-// app.post("/ask", async (req, res) => {
-//   const { question } = req.body;
-
-//   try {
-//     const response = await client.responses.create({
-//       model: "gpt-4.1-mini",
-//       input: question
-//     });
-
-//     res.json({ answer: response.output_text });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-
-// app.listen(5000, () => {
-//   console.log("Server running on port 5000");
-// });
 import express from "express";
 import axios from "axios";
 import cors from "cors";
@@ -53,9 +10,7 @@ console.log("OpenRouter Key:", process.env.OPENROUTER_API_KEY);
 
 const app = express();
 
-app.use(cors({
-  origin: "http://localhost:5173"
-}));
+app.use(cors());
 
 app.use(express.json());
 
